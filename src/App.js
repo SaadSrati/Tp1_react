914c17463a0dc703dd1f1a56da4c5f2c1d30b57a
saad srati <saadsrati2004@gmail.com>
2023-11-26 20:45:29 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Calc from './compenent/Calc';
+import './compenent/style.css'
+function App() {
+  return (
+    <div className="App">
+  
+    <Calc/>
+    </div>
+  );
+}
+
+export default App;
